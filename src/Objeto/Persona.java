b44ac7840b71fde36cc183ba1df7035fa62e6e45
Maria Martinez <mmariayennifer.25@gmail>
2023-08-10 19:10:55 -0500

    se agrega el proyecto de Objeto

---
+++ src/Objeto/Persona.java
@@ -0,0 +1,61 @@
+package Objeto;
+
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String identificacion;
+
+    // Constructor vacío
+    public Persona() {
+    }
+
+    // Constructor con todos los atributos
+    public Persona(String nombre, int edad, String identificacion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.identificacion = identificacion;
+    }
+
+    // Métodos getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    // Métodos setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        if (edad >= 0) {
+            this.edad = edad;
+        } else {
+            System.out.println("La edad no puede ser negativa.");
+        }
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    // Método para mostrar los datos de la persona
+    public void mostrar() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Identificación: " + identificacion);
+    }
+
+    // Método para verificar si es mayor de edad
+    public boolean esMayorDeEdad() {
+        return edad >= 18;
+    }
+}
