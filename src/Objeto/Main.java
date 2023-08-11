198decb046b7598ad2f70e78aceb826be69ebc0a
Maria Martinez <mmariayennifer.25@gmail>
2023-08-10 21:12:57 -0500

    creando claPer creando Objeto
    Signed-off-by: Maria Martinez <mmariayennifer.25@gmail>

--- src/Objeto/Main.java
+++ src/Objeto/Main.java
@@ -2,6 +2,11 @@ package Objeto;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona persona1= new Persona();
+        persona1.setNombre("Maria");
+        persona1.setEdad(10);
+        persona1.setIdentificacion("PT1020228");
+        persona1.mostrar();
+        persona1.esMayorDeEdad();
     }
 }
